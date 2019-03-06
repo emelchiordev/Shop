668502866534e4d9833dbef63c0e30ce548c0c73
odyssee974 <manu.melchior@gmail.com>
2019-03-06 16:57:54 +0100

    init directory

---
+++ src/Bill.java
@@ -0,0 +1,20 @@
+import java.util.Map;
+
+public class Bill {
+    private Customer customer;
+    private Map<Product,Integer> product;
+
+    public Bill(){
+        this.customer = customer;
+    }
+
+    public void addProduct(Product product, Integer quantity){
+        this.product.put(product,quantity);
+    }
+    public Customer getCustomer(){
+        return customer;
+    }
+    public Map<Product, Integer> getProucts(){
+        return product;
+    }
+}
---
+++ src/Product.java
@@ -0,0 +1,41 @@
+public class Product {
+    private String name;
+    private String description;
+    private double price;
+
+    public Product(String name, String description, double price){
+      this.name = name;
+      this.description = description;
+      this.price = price;
+    }
+
+
+    /**
+     * Display a full description of the product
+     */
+    public void look(){
+    }
+    /**
+    * Add product to a Bill
+     * @param bill the concerned bill
+     * @param quantity quantity to add
+     */
+    public void buy(Bill bill, Integer quantity){
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
