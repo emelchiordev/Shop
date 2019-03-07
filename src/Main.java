21adcae6f217f530e1d7a3ef2283e56c35fbca4c
odyssee974 <manu.melchior@gmail.com>
2019-03-07 17:21:41 +0100

    Ajout produit dans la facture

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,12 @@ public class Main {
         Customer melchior = new Customer("MELCHIOR HENRI EMMANUEL","11 RUE YVES MANCEAU \n49330 QUERRE");
         System.out.println(melchior.getFullname());
         System.out.println(melchior.getAddress());
+        Television Samsung1 = new Television("TV SAMSUNG EHD","TV INCURVED",5.30,55,"incurved");
         Product coca = new Product("Coca","Boison gazeuse",5.30);
         Bill facture1 = new Bill(melchior);
         facture1.addProduct(coca,2);
+        facture1.addProduct(Samsung1,1);
+
 
 
     }
